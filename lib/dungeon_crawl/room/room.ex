404c16686b8d0c7d48d3ff0621bd7dd13d856eab
Rhiad Ciccoli <rhiad.ciccoli@gmail.com>
2021-07-07 13:01:46 -0300

    create triggers and exit trigger

--- lib/dungeon_crawl/room/room.ex
+++ lib/dungeon_crawl/room/room.ex
@@ -1,11 +1,17 @@
 defmodule DungeonCrawl.Room do
     alias DungeonCrawl.Room
+    alias DungeonCrawl.Room.Triggers
 
     import DungeonCrawl.Room.Action
 
-    defstruct description: nil, actions: []
+    defstruct description: nil, actions: [], trigger: nil
 
     def all, do: [
+        %Room{
+            description: "You can see the light of the day. You found the exit!",
+            actions: [forward()],
+            trigger: Triggers.exit
+        },
         %Room{
             description: "You found a quiet place. Looks safe for a little nap",
             actions: [forward(), rest()]
